3cfd4f7fe67891f8f0c32837b4903a8f1e7ca7dd
Rampop01 <popoolarahmat96@gmail.com>
2025-12-09 16:36:36 +0100

    tried flatlist instead of map

--- components/components/ColorPicker.tsx
+++ components/components/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, FlatList } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 
@@ -10,12 +10,21 @@ const [color, setColor] = useState('blue');
     <View style={{padding: 20 , backgroundColor: color, height: '100%'}}>
         <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' , textTransform: 'uppercase', color: 'white'}}>Select a Color</Text>
         <View >
-            <View style={{flexDirection: 'row'}}>
+            {/* <View style={{flexDirection: 'row'}}>
               {colors.map((color) => (<Pressable key={color} onPress={()=> setColor(color)} style={{backgroundColor: color, padding: 10, margin: 5 , height: 50, width: 50, borderRadius: 50, borderColor: "black"}}>{color}</Pressable> ))}
-            </View>
+            </View> */}
+
+            <FlatList 
+              data={colors}
+              keyExtractor={(index) => index.toString()}
+              renderItem={({item}) => (<Pressable onPress={()=> setColor(item)} style={{backgroundColor: item, padding: 10, margin: 5 , height: 50, width: 50, borderRadius: 50, borderColor: "black"}}></Pressable>)}
+      horizontal={true}
+     />
         </View>
     </View>
   )
 }
+
+{/*   */}
 
 export default ColorPicker
